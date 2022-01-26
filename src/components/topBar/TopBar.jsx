4e3379d8035495bca8f266aa1d9bb13b8f21341e
Jason Hoang <jasonhoang6201@gmail.com>
2022-01-26 18:08:49 +0700

    menu topbar

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,5 +1,5 @@
 import './TopBar.scss'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaAngleRight } from 'react-icons/fa'
 
 function TopBar(props) {
     return (
@@ -16,10 +16,10 @@ function TopBar(props) {
                     <li className="topbar_content_item">
                         <a href="/#">PACKAGES</a>
                         <ul className='topbar_item_root_list'>
-                            <li>Search 1 <span>HOT</span></li>
+                            <li>Search 1 <span className='topbar_item_root_badge'>HOT</span></li>
                             <li>Search 2</li>
                             <li>
-                                Tour Package
+                                Tour Package <span><FaAngleRight /></span>
                                 <ul className='topbar_item_children_list'>
                                     <li>Carousel <span>BEST</span></li>
                                     <li>Custom Map</li>
@@ -33,18 +33,68 @@ function TopBar(props) {
                     </li>
                     <li className="topbar_content_item">
                         <a href="/#">SHOP</a>
+                        <ul className='topbar_item_root_list'>
+                            <li>Archive</li>
+                            <li>Single Product</li>
+                            <li>Cart</li>
+                            <li>Checkout</li>
+                        </ul>
                     </li>
                     <li className="topbar_content_item">
                         <a href="/#">ABOUT US</a>
+                        <ul className='topbar_item_root_list'>
+                            <li>About us <span className="topbar_item_root_badge">NEW</span></li>
+                            <li>About us 1</li>
+                            <li>About us 2</li>
+                        </ul>
                     </li>
                     <li className="topbar_content_item">
                         <a href="/#">PAGES</a>
+                        <ul className="topbar_item_root_list">
+                            <li>Services</li>
+                            <li>Agency Tours</li>
+                            <li>Testimonials</li>
+                            <li>Prices</li>
+                            <li>Promotions</li>
+                            <li>Faq</li>
+                            <li>Coming Soon</li>
+                            <li>
+                                About us <span><FaAngleRight /></span>
+                                <ul className="topbar_item_children_list">
+                                    <li>About us</li>
+                                    <li>About us 1</li>
+                                    <li>About us 2</li>
+                                </ul>
+                            </li>
+                            <li>
+                                Contact <span><FaAngleRight /></span>
+                                <ul className="topbar_item_children_list">
+                                    <li>Contact 1</li>
+                                    <li>Contact 2</li>
+                                </ul>
+                            </li>
+                        </ul>
                     </li>
                     <li className="topbar_content_item">
                         <a href="/#">NEWS</a>
+                        <ul className="topbar_item_root_list">
+                            <li>Archive</li>
+                            <li>
+                                Single Post <span><FaAngleRight /></span>
+                                <ul className="topbar_item_children_list">
+                                    <li>Full width</li>
+                                    <li>Right Sidebar</li>
+                                    <li>Left Sidebar</li>
+                                </ul>
+                            </li>
+                        </ul>
                     </li>
                     <li className="topbar_content_item">
                         <a href="/#">CONTACT</a>
+                        <ul className="topbar_item_root_list">
+                            <li>Contact 1</li>
+                            <li>Contact 2</li>
+                        </ul>
                     </li>
                     <li className="topbar_content_item topbar_content_book_now">
                         <a href="/#">BOOK NOW</a>
