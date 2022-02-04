f84c11a1fe23b3cf3754610fb8bff6327195e2c6
Jason Hoang <jasonhoang6201@gmail.com>
2022-02-04 23:55:32 +0700

    done search1 UI

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,11 +9,81 @@ import { Row, Col } from 'react-bootstrap'
 import { useState } from 'react'
 import LeftMenu from '../leftMenu/LeftMenu'
 
+class Calender {
+    static currentYear = new Date(Date.now()).getFullYear()
+    static currentMonth = new Date(Date.now()).getMonth() + 1
+    static currentDay = new Date(Date.now()).getDate()
+
+    static isLeapYear(year) {
+        return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    }
+
+    static getFebruary(year) {
+        return this.isLeapYear(year) ? 29 : 28
+    }
+
+    static firstPos(month, year) {
+        return new Date(month, year, 1).getDay()
+    }
+
+    static months = ['January', 'February', "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    static displayTable(month, year) {
+        let days = [31, this.getFebruary(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        let element = []
+        let dayCount = 0
+
+        for (let i = 0; i <= days[month - 1] + this.firstPos(month, year); i) {
+            let temp = []
+            for (let j = 0; j < 7; j++) {
+                if (i < this.firstPos(month, year) || dayCount > days[month - 1] - 1) {
+                    temp.push('')
+                    i++
+                } else {
+                    temp.push(++dayCount)
+                    i++
+                }
+            }
+            element.push(temp)
+        }
+        return element
+    }
+}
+
 function Header(props) {
     const [leftMenu, setLeftMenu] = useState(false)
 
     const openLeftMenu = () => setLeftMenu(true)
     const closeLeftMenu = () => setLeftMenu(false)
+
+    //calender
+    const [isShowCalender, setIsShowCalender] = useState(false)
+    const [day, setDay] = useState(Calender.currentDay)
+    const [month, setMonth] = useState(Calender.currentMonth)
+    const [year, setYear] = useState(Calender.currentYear)
+
+    const display = Calender.displayTable(month, year)
+
+    const handlePrevMonth = () => {
+        if (month === 1) {
+            setYear(year - 1)
+            setMonth(12)
+            return
+        } else setMonth(month - 1)
+    }
+    const handleNextMonth = () => {
+        if (month === 12) {
+            setYear(year + 1)
+            setMonth(1)
+            return;
+        } else setMonth(month + 1)
+    }
+
+    const handleChooseDay = event => {
+        setDay(event.target.textContent)
+        setIsShowCalender(false)
+    }
+
 
     return (
         <>
@@ -81,18 +151,67 @@ function Header(props) {
                                     <div className="header_search_filter_section">
                                         <h6>Select Your Destination</h6>
                                         <div className="header_search_filter_section_input">
-                                            All destinations
-                                            <div><FaMapMarkerAlt /></div>
+                                            <div className='header_search_filter_section_input_icon'><FaMapMarkerAlt /></div>
+                                            <select>
+                                                <option value="">All destination</option>
+                                                <option value="">Europe</option>
+                                                <option value="">Italy</option>
+                                                <option value="">Netherlands</option>
+                                                <option value="">Asia</option>
+                                                <option value="">Thailand</option>
+                                                <option value="">United State</option>
+                                                <option value="">Oceania</option>
+                                            </select>
                                         </div>
+
                                     </div>
                                 </Col>
                                 <Col md={4} className='justify-content-center d-flex'>
                                     <div className="header_search_filter_section">
                                         <h6>Select Your Date</h6>
-                                        <div className="header_search_filter_section_input">
-                                            mm-dd-yyyy
-                                            <div><FaCalendarAlt /></div>
+                                        <div className='header_search_filter_section_date'>
+                                            <div className='header_search_filter_section_date_input' onClick={() => setIsShowCalender(!isShowCalender)}>
+                                                {`${day}-${month}-${year}`}
+                                                <div className='header_search_filter_section_input_icon'><FaCalendarAlt /></div>
+                                            </div>
+
+                                            {isShowCalender &&
+                                                <div className="header_search_filter_section_date_calender">
+                                                    <div className="header_search_filter_section_date_calender_top">
+                                                        <button onClick={handlePrevMonth}>PREV</button>
+                                                        <div className='header_search_filter_section_date_calender_top_header'>{Calender.months[month - 1]} {year}</div>
+                                                        <button onClick={handleNextMonth}>NEXT</button>
+                                                    </div>
+                                                    <table className="header_search_filter_section_date_calender_table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Sun</th>
+                                                                <th>Mon</th>
+                                                                <th>Tue</th>
+                                                                <th>Wed</th>
+                                                                <th>Thu</th>
+                                                                <th>Fri</th>
+                                                                <th>Sat</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            {display.map((item, index) => {
+                                                                return (
+                                                                    <tr key={index}>
+                                                                        {item.map((it, i) => {
+                                                                            if (it < Calender.currentDay && month === Calender.currentMonth && year === Calender.currentYear) return <th key={i} className='calender_passed_days' onClick={event => handleChooseDay(event)}>{it}</th>
+                                                                            if (it === Calender.currentDay && month === Calender.currentMonth && year === Calender.currentYear) return <th key={i} className='calender_current_day' onClick={event => handleChooseDay(event)}>{it}</th>
+                                                                            else return <th key={i} onClick={event => handleChooseDay(event)}>{it}</th>
+
+                                                                        })}
+                                                                    </tr>
+                                                                )
+                                                            })}
+                                                        </tbody>
+                                                    </table>
+                                                </div>}
                                         </div>
+
                                     </div>
                                 </Col>
                                 <Col md={4} className='justify-content-center d-flex'>
@@ -119,12 +238,28 @@ function Header(props) {
 
                         <div className="header_options">
                             <div className="header_options_section">
-                                <span>PRICE <FaAngleDown /></span>
-                                <span>NAME <FaAngleDown /></span>
+                                <span className='header_options_section_item'>
+                                    PRICE <FaAngleDown />
+                                    <div className="header_option_filter">
+                                        <ul>
+                                            <li>LOWEST PRICE</li>
+                                            <li>HIGHT PRICE</li>
+                                        </ul>
+                                    </div>
+                                </span>
+                                <span className='header_options_section_item'>
+                                    NAME <FaAngleDown />
+                                    <div className="header_option_filter">
+                                        <ul>
+                                            <li>DESC</li>
+                                            <li>ASC</li>
+                                        </ul>
+                                    </div>
+                                </span>
                             </div>
                             <div className="header_options_section">
-                                <span><FaListUl /></span>
-                                <span><FaEllipsisH /></span>
+                                <span className={`header_options_section_item ${!props.isList && 'unselected_item'}`} onClick={props.handleSetDisplayList}><FaListUl /></span>
+                                <span className={`header_options_section_item ${props.isList && 'unselected_item'}`} onClick={props.handleSetDisplayList2}><FaEllipsisH /></span>
                             </div>
                         </div>
                     </div>
--- src/pages/search1/Search1.jsx
+++ src/pages/search1/Search1.jsx
@@ -1,10 +1,97 @@
+import { useState } from 'react';
 import Header from '../../components/header/Header';
 import './Search1.scss'
+import { Row, Col } from 'react-bootstrap'
+import Package from '../../components/package/Package';
 
 function Search1() {
+    const [isList, setIsList] = useState(false);
+
+    const handleSetDisplayList = () => setIsList(true)
+    const handleSetDisplayList2 = () => setIsList(false)
+
+    const yellowGradientColor = 'linear-gradient(to right, #ffd205 0%, #ff9b05 100%)'
+    const redGradientColor = 'linear-gradient(to right, #f76570 0%, #f78d65 100%)'
+    const purpleGradientColor = 'linear-gradient(to right, #ba71da 0%, #da717b 100%)'
+
+    const data = [
+        {
+            image: './assets/image/package-1-1024x640.jpg',
+            icon: './assets/icon/arch.png',
+            name: 'Berlin',
+            location: 'Europe',
+            category: { first: 'CULTURAL', second: 'RELAX' },
+            price: 700,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: yellowGradientColor
+        },
+        {
+            image: './assets/image/package-6-1024x640.jpg',
+            icon: './assets/icon/torii-gate.png',
+            name: 'Hong Kong',
+            location: 'Asia',
+            category: { first: 'HISTORY', second: 'CULTURAL' },
+            rootPrice: 1000,
+            price: 600,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: redGradientColor
+        },
+        {
+            image: './assets/image/package-2-1024x640.jpg',
+            icon: './assets/icon/golden-gate.png',
+            name: 'San Francisco',
+            location: 'United Stated',
+            category: { first: 'SPORT', second: 'RELAX' },
+            price: 400,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: purpleGradientColor
+        },
+        {
+            image: './assets/image/package-3-1024x640.jpg',
+            icon: './assets/icon/golden-gate.png',
+            name: 'Phu Ket',
+            location: 'ThaiLand',
+            category: { first: 'RELAX', second: 'CULTURAL' },
+            price: 1200,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: yellowGradientColor
+        },
+        {
+            image: './assets/image/package-4-1024x640.jpg',
+            icon: './assets/icon/torii-gate.png',
+            name: 'Amsterdam',
+            location: 'Netherlands',
+            category: { first: 'HISTORY', second: 'SPORT' },
+            price: 1500,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: redGradientColor
+        },
+        {
+            image: './assets/image/package-5-1024x640.jpg',
+            icon: './assets/icon/golden-gate.png',
+            name: 'Tuscany',
+            location: 'Italy',
+            category: { first: 'SPORT', second: 'RELAX' },
+            rootPrice: 950,
+            price: 730,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut efficitur ante. Donec dapibus dictum scelerisque.',
+            color: purpleGradientColor
+        }
+    ]
+
     return (
         <>
-            <Header search1={true} />
+            <Header search1={true} isList={isList} handleSetDisplayList={handleSetDisplayList} handleSetDisplayList2={handleSetDisplayList2} />
+
+            <Row className='p-md-5 p-3 '>
+                {data.map((item, index) => {
+                    return (
+                        <Col md={4} key={index} className='search1_packages_item' >
+                            <Package packages={item} />
+                        </Col>
+                    )
+                })}
+            </Row>
         </>
     );
 }
